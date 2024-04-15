89470ae76a51b70d64acab1c9d86592a0bdbad56
Valery Makey <valerymakkol@gmail.com>
2024-04-15 15:29:30 +0300

    Final responsiveness added

--- src/components/Extra/Extra.jsx
+++ src/components/Extra/Extra.jsx
@@ -3,8 +3,9 @@ import React from "react";
 import styles from "./styles.module.css";
 import Block from "../Block/Block";
 
-import scorpio from "../../assets/scorpio.png";
-import quotes from "../../assets/quotes.png";
+import scorpio from "../../assets/scorpio.svg";
+import digit from "../../assets/digit.svg";
+import quotes from "../../assets/quotes.svg";
 
 const Extra = () => {
   return (
@@ -12,7 +13,7 @@ const Extra = () => {
       <h2>Дополнительно</h2>
       <div className={styles.extraWrapper}>
         <Block image={scorpio} />
-        <Block image={scorpio} />
+        <Block image={digit} />
         <Block image={quotes} />
       </div>
     </>
