dd8f59d8f70753587442d8c57db7236cf9f2b0b4
Valery Makey <valerymakkol@gmail.com>
2024-04-20 00:24:08 +0300

    Test 2.6

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,25 +7,27 @@ import heroImage from "../../assets/heroImage.png";
 const Main = () => {
   return (
     <>
-      <div className={styles.mainWrapper}>
-        <div className={styles.heroContainer}>
-          <div className={styles.heroText}>
-            <h1 className={styles.heroHeading}>Верь в своё будущее</h1>
-            <p className={styles.heroParagraph}>
-              Monteia - это сервис для гаданий, нумерологии и гороскопов
-            </p>
-            <Button
-              content={"список гаданий"}
-              icon={"arrow"}
-              size={"big"}
-              iconWidth={"wide"}
-            />
+      <div className={styles.mainBackground}>
+        <div className={styles.mainWrapper}>
+          <div className={styles.heroContainer}>
+            <div className={styles.heroText}>
+              <h1 className={styles.heroHeading}>Верь в своё будущее</h1>
+              <p className={styles.heroParagraph}>
+                Monteia - это сервис для гаданий, нумерологии и гороскопов
+              </p>
+              <Button
+                content={"список гаданий"}
+                icon={"arrow"}
+                size={"big"}
+                iconWidth={"wide"}
+              />
+            </div>
+            <img className={styles.image} src={heroImage} alt="Aroma Stick" />
           </div>
-          <img className={styles.image} src={heroImage} alt="Aroma Stick" />
-        </div>
-        <div className={styles.heroScroll}>
-          <Icon scalable={1} type={"mouse"} status={0} width={"regular"} />
-          <p className={styles.scrollParagraph}>листайте ниже</p>
+          <div className={styles.heroScroll}>
+            <Icon scalable={1} type={"mouse"} status={0} width={"regular"} />
+            <p className={styles.scrollParagraph}>листайте ниже</p>
+          </div>
         </div>
       </div>
     </>
