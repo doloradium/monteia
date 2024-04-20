cb78ceaddc10b5ae16fd2a0151b89198831702d0
Valery Makey <valerymakkol@gmail.com>
2024-04-20 11:50:27 +0300

    Test 2.9

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,10 +3,22 @@ import styles from "./styles.module.css";
 import { Icon } from "../Icon/Icon";
 import Button from "../Button/Button";
 import heroImage from "../../assets/heroImage.png";
+import { useEffect } from "react";
 
 const Main = () => {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  });
+
   return (
     <>
+      <div className={styles.mainBackground}></div>
       <div className={styles.mainWrapper}>
         <div className={styles.heroContainer}>
           <div className={styles.heroText}>
